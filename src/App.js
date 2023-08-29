e3f8ffbbc613e31f67a57cc1ea57057449acafd5
rajeshofficial <rscdraj@gmail.com>
2023-08-29 12:34:50 +0100

    Merge/rebase remote changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import cover from './images /cover.jpeg'
 import profile from './images /profile.avif'
 // import React, { useState } from 'react';
 
-const contactData = `
+const data = {
+  contactData: `
 BEGIN:VCARD
 VERSION:3.0
 N:Alex;Decla
@@ -11,63 +12,60 @@ ORG:Axel Group
 TEL:+44 9999211391 
 EMAIL:declan@axlegroup.uk
 END:VCARD
-`;
-const linkData = {
-  Instagram : "https://www.instagram.com/",
-  linkedin : "https://www.linkedin.com/",
-  website: "https://www.google.com",
- 
-}
-function App() {
-  // const [displayText, setDisplayText] = useState('Send Messsge');
+`,
+  linkData: {
+    Instagram: "https://www.instagram.com/",
+    linkedin: "https://www.linkedin.com/",
+    website: "https://www.google.com",
+  },
+};
 
-  const showContact  = () => {
-    const telMatch = contactData.match(/TEL:(.+)/);
+function App() {
+  const showContact = () => {
+    const telMatch = data.contactData.match(/TEL:(.+)/);
     if (telMatch) {
       const telNumber = telMatch[1].trim();
-      // setDisplayText(telNumber);
       window.location.href = `sms:${telNumber}`;
-    } 
+    }
+  };
 
-  }; 
-  
- 
-  const downloadContact = () => {                                       // This function is called when the "Save Contact" button is clicked
-                                                                       // Contact information formatted as a vCard
-
-    // Creating a Blob with the contact data and specifying the MIME type
-    const blob = new Blob([contactData], { type: 'text/vcard' });
-
-    // Creating a URL for the Blob to enable downloading
+  const downloadContact = () => {
+    const blob = new Blob([data.contactData], { type: 'text/vcard' });
     const url = URL.createObjectURL(blob);
-
-    // Creating a temporary link element for downloading
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'contact.vcf'; // Setting the download filename
+    link.download = 'contact.vcf';
     document.body.appendChild(link);
-
-    // Simulating a click on the link to trigger the download
     link.click();
-
-    // Removing the temporary link element and releasing the URL
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
   };
-  
-  const callFromContact = () => {
-    // Regular expression to match TEL field in vCard
-    const telRegex = /TEL:(\+[0-9]+)\b/;
 
-    // Extracting phone number using regular expression
-    const match = contactData.match(telRegex);
+  // const callFromContact = () => {
+  //   const telRegex = /TEL:(\+?[0-9]+)\b/; // Updated regex to handle optional plus sign
+  //   const match = data.contactData.match(telRegex);
+  //   if (match && match[1]) {
+  //     const phoneNumber = match[1];
+  //     const cleanedPhoneNumber = phoneNumber.replace(/\s/g, ''); // Remove spaces
+  //     window.location.href = `tel:${cleanedPhoneNumber}`;
+  //   } else {
+  //     console.log("Phone number not found in vCard data");
+  //   }
+  // };
+  const callFromContact = () => {
+    const telRegex = /TEL:(\+?[0-9\s]+)\b/; // Updated regex to handle optional plus sign and spaces
+    const match = data.contactData.match(telRegex);
     if (match && match[1]) {
       const phoneNumber = match[1];
-      window.location.href = `tel:${phoneNumber}`;
+      const cleanedPhoneNumber = phoneNumber.replace(/\s/g, ''); // Remove spaces
+      window.location.href = `tel:${cleanedPhoneNumber}`;
     } else {
       console.log("Phone number not found in vCard data");
     }
   };
+  
+
+  
 
   return (
     <div className="card">
@@ -75,22 +73,22 @@ function App() {
       <img className="profile-image" src={profile} alt="Declan, CEO & Founder of Alex Group" />
 
       <h1>Declan</h1>
-      <p className="title">CAlex Group's CEO & Founder leads with vision, fostering teamwork and innovation, propelling the company to remarkable success and technological advancement.</p>
+      <p className="title">Alex Group's CEO & Founder leads with vision, fostering teamwork and innovation, propelling the company to remarkable success and technological advancement.</p>
       <p>Strathclyde University</p>
       <p className="btns">
-        <button onClick={showContact}>{`Send message`}</button>
-        <a href={linkData.website} target="_blank" rel="noopener noreferrer">
-        <button>Website </button>
+        <button onClick={showContact}>Send message</button>
+        <a href={data.linkData.website} target="_blank" rel="noopener noreferrer">
+          <button>Website</button>
         </a>
-        <a href={linkData.Instagram} target="_blank" rel="noopener noreferrer">
-        <button>Instagram </button>
+        <a href={data.linkData.Instagram} target="_blank" rel="noopener noreferrer">
+          <button>Instagram</button>
         </a>
-        <a href={linkData.linkedin} target="_blank" rel="noopener noreferrer">
-        <button> Linkedin  </button>
+        <a href={data.linkData.linkedin} target="_blank" rel="noopener noreferrer">
+          <button>LinkedIn</button>
         </a>
       </p>
-      <div class="btns2">
-        <button onClick={downloadContact}> Save Contact</button>
+      <div className="btns2">
+        <button onClick={downloadContact}>Save Contact</button>
         <button onClick={callFromContact}>Call</button>
       </div>
     </div>
